00f5884fafba4b9733b9cf9a7a56e791a0345029
kyle <truongondaihoc@gmail.com>
2023-04-20 15:15:47 +0700

    update error

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("Hello");
+// import {} from "fp-ts";
+import * as EI from "fp-ts/lib/Either";
+import { pipe } from "fp-ts/lib/function";
+
+interface IUser {
+  name: string;
+}
+
+type UserError = {
+  error: string;
+};
+
+const k = EI.fromNullable(5);
+function getUserName(): EI.Either<UserError, IUser> {
+  if (Math.random() > 0.5) return EI.left({ error: "hello" });
+  return EI.right({ name: "Chao xin" });
+}
+
+pipe(EI.Do, getUserName, console.log);
